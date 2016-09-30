89b322c6a0e4616d9baa5fdaba24a7d5aa59d708
wangyang <357466524@qq.com>
2016-10-01 01:01:43 +0800

    push

--- app/web/callback.php
+++ app/web/callback.php
@@ -7,11 +7,12 @@
 <?php
 // error_reporting (E_ALL & ~E_NOTICE);
 // error_log("error_message", 3, "log.txt");
-$uri = "https://getpocket.com/v3/oauth/request";
+$uri = "https://getpocket.com/v3/oauth/authorize";
+$request_token = $_GET['request_token'];
 // 参数数组
 $data = array (
     'consumer_key' => '58985-37359df551b6a46182944f93', 
-    'redirect_uri' => 'btc.xiaxiatao.com/callback.php' 
+    'code' => $request_token 
 );
  
 $ch = curl_init ();
@@ -20,8 +21,8 @@ curl_setopt ( $ch, CURLOPT_URL, $uri );
 curl_setopt ( $ch, CURLOPT_POST, 1 );
 curl_setopt ( $ch, CURLOPT_HEADER, 0 );
 curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查 
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true); // 从证书中检查SSL加密算法是否存在
+curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查 
+curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, true); // 从证书中检查SSL加密算法是否存在
 curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
     'Content-Type: application/json; charset=UTF-8',
     "X-Accept: application/json") 
