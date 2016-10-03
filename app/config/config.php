256ddd42e438c62684d2e19df8ef9ce3b1cd0c3d
wangyang <357466524@qq.com>
2016-10-03 11:56:06 +0800

    namespace

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+namespace PocketExample\Config;
+/**
+ * mysql配置
+ * @author walkor
+ */
+class Config
+{
+    public static $consumer_key = '58985-37359df551b6a46182944f93';
+}
--- app/web/list.php
+++ app/web/list.php
@@ -1,4 +1,7 @@
 <?php
+use PocketExample\Common\Poster;
+use PocketExample\Config\Config;
+
 	session_start();
 
 	$name = $_SESSION['name'];
@@ -7,28 +10,14 @@
 	if(!isset($_SESSION['access_token'])){
 		header("Location:/login.php");
 	}
-	$consumer_key = '58985-37359df551b6a46182944f93';
+	//$consumer_key = '58985-37359df551b6a46182944f93';
 
-	$data['consumer_key'] = $consumer_key;
+	$data['consumer_key'] = Config::$consumer_key;
 	$data['access_token'] = $token;
 	$data['count'] = 100;
 	$data['state'] = 'all';
 	$uri = 'https://getpocket.com/v3/get';
-	$ch = curl_init ();
-	// print_r($ch);
-	curl_setopt ( $ch, CURLOPT_URL, $uri );
-	curl_setopt ( $ch, CURLOPT_POST, 1 );
-	curl_setopt ( $ch, CURLOPT_HEADER, 0 );
-	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查 
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true); // 从证书中检查SSL加密算法是否存在
-	curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
-	    'Content-Type: application/json; charset=UTF-8',
-	    "X-Accept: application/json") 
-	);
-	curl_setopt ( $ch, CURLOPT_POSTFIELDS, json_encode($data) );
-	$return = curl_exec ( $ch );
-	curl_close ( $ch );
+	$return = Poster::post($uri, $data);
 	$ret = json_decode($return, true);
 	var_dump($ret);
 	$list = $ret['list'];
